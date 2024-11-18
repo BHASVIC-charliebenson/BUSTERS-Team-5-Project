86d10d2237046f2912edf4a2c06c8d996b77aa7a
BHASVIC-AlkonyNassir07 <alkony.nassir24@students.bhasvic.ac.uk>
2024-11-18 12:29:29 +0000

    d

--- Check out.c
+++ Check out.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+
 
 int main(void){
-    int adults,children,staylength,choice;
+    int adults = 1,children = 1,staylength,choice;
     float roomtotal = 0,mealtotal = 0,totalcost = 0;
     char type,paper;
-    printf("");
+    printf("what is type");
     fflush(stdin);
     scanf("%c",&type);
-    switch(toupper(type)) {
-        case 'F':
-            mealtotal+=20;
-        break;
-        case 'H':
-            mealtotal+=15;
-        break;
-        case 'B':
-            mealtotal+=5;
+    printf("what is paper");
+    fflush(stdin);
+    scanf("%c",&paper);
+    if (type == 'F' || type == 'f') {
+        mealtotal += 20;
+    }
+    else if (type == 'H' || type == 'h') {
+        mealtotal += 20;
+    }
+    if (type == 'F' || type == 'f') {
+        mealtotal += 5;
+    }
         break;
         default:
-            printf("%c is not valid",toupper(type));
+            printf("%c is not a valid meal type\n", toupper(type));
         break;
     }
-    mealtotal = mealtotal(children/2*adults);
-    switch(toupper(paper)) {
+    mealtotal = mealtotal*(children/2)*adults;
+    switch (toupper(paper)) {
         case 'Y':
-            totalcost+=5.5;
+            totalcost += 5.5;
         break;
         case 'N':
-            totalcost = totalcost;
-        break;
+            break;
         default:
-            printf("%c is not valid",toupper(paper));
+            printf("%c is not a valid paper option\n", toupper(paper));
         break;
     }
     
     printf("%d",mealtotal);
+    printf("%d",totalcost);
     return 0;
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/eddiechow/Documents/GitHub/BUSTERS-Team-5-Project
+# Install script for directory: C:/Users/alkony.nassir24/computing/BUSTERS-Team-5-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/BUSTERS_Team_5_Project_BHASVIC_EddieChow24")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/Program Files (x86)/JetBrains/CLion 2024.1.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/eddiechow/Documents/GitHub/BUSTERS-Team-5-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/alkony.nassir24/computing/BUSTERS-Team-5-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
